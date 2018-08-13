aea7c6adf8f98106dc68f6b5f5128fc75aa24b08
Lotus <cmadisonne@gmail.com>
2018-08-13 10:29:42 -0500

    Done

---
+++ javascript.js
@@ -0,0 +1,12 @@
+document.getElementById("headlineID").innerText="Changing the text for Exercise 1";
+
+document.getElementById("HeadlineClassID").innerHTML='<header>' +
+    '<h2><a href="generic.html">Ipsum lorem sed magna Maddi Coope</a></h2>' +
+    '<p class="info">3 days ago by <a href="#">Jane Doe</a></p>' +
+    '</header>' +
+    '<p>Nullam posuere erat vel placerat rutrum. Praesent ac consectetur dui, et congue quam. Donec aliquam lacinia condimentum. Integer porta massa sapien, commodo sodales diam suscipit vitae. Aliquam quis felis sed urna semper semper. Phasellus eu scelerisque mi. Vivamus aliquam nisl libero, sit amet scelerisque ligula laoreet vel.</p>' +
+    '<ul class="actions">' +
+    '<li><a href="generic.html" class="button alt">Read More</a></li>' +
+    '</ul>';
+
+document.getElementById("kennButton").innerText='Changing text for Ex3';
